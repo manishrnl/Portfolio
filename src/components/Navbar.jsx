a6acd61c13b4eca46cc873bba18d9d8d1ace1d06
Manish Kumar <79047406+manishrnl@users.noreply.github.com>
2025-05-18 19:12:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            MANISH <span className="sm:block hidden"> | Java Developer</span>
+            MANISH <span className="sm:block hidden">  |  Java Developer </span>
           </p>
         </Link>
 
