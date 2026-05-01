7bbfb3949fe335ad67d17d4141c2a3cac1f83382
Manish Kumar <manishrajrnl@zohomail.in>
2026-05-01 16:54:42 +0530

    Update ResumeLink to new Google Drive URL

--- src/utils/Resume-Data.js
+++ src/utils/Resume-Data.js
@@ -1,1 +1,1 @@
-export const ResumeLink ="https://workdrive.zoho.in/file/168gj7f3eb4921443497b9ed720215e99d68a";
+export const ResumeLink ="https://drive.google.com/file/d/1GGUSocQ7LRALMAnLTy_4BLZEsSuMzbsI/view?usp=drivesdk";
